abbe516b83e4730b4f0a7d3d03ed924b95108d02
bipinemp <bhandaribipin3.39@gmail.com>
2023-04-27 22:22:49 +0545

    Day1 changes

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.readFile(
+  path.join(__dirname, "files", "starter.txt"),
+  "utf8",
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }
+);
+
+console.log("Hello...");
+
+fs.writeFile(
+  path.join(__dirname, "files", "reply.txt"),
+  "Nice to meet you",
+  (err) => {
+    if (err) throw err;
+    console.log("Write complete");
+  }
+);
+
+fs.appendFile(
+  path.join(__dirname, "files", "test.txt"),
+  "Testing text",
+  (err) => {
+    if (err) throw err;
+    console.log("Append complete");
+  }
+);
+
+//exit on uncaught errors
+process.on("uncaughtException", (err) => {
+  console.error(`There was an uncaught error": ${err}`);
+  process.exit(1);
+});
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 // console.log("hello world");
 
-const os = require("os");
-const path = require("path");
+// const os = require("os");
+// const path = require("path");
+// const { add, subtract, multiply, divide } = require("./math");
 
-console.log(os.type()); //Windows_NT
-console.log(os.version()); //Windows 10 Home
-console.log(os.homedir()); // C:\Users\bhandari
+// console.log(add(1, 2));
+// console.log(subtract(1, 2));
+// console.log(multiply(1, 2));
+// console.log(divide(1, 2));
 
-console.log(__dirname); //D:\NodeJsLearning
-console.log(__filename); //D:\NodeJsLearning\server.js
+// console.log(os.type()); //Windows_NT
+// console.log(os.version()); //Windows 10 Home
+// console.log(os.homedir()); // C:\Users\bhandari
 
-console.log(path.dirname(__filename)); // D:\NodeJsLearning
-console.log(path.basename(__filename)); //server.js
-console.log(path.extname(__filename)); //.js
+// console.log(__dirname); //D:\NodeJsLearning
+// console.log(__filename); //D:\NodeJsLearning\server.js
+
+// console.log(path.dirname(__filename)); // D:\NodeJsLearning
+// console.log(path.basename(__filename)); //server.js
+// console.log(path.extname(__filename)); //.js
+
+// console.log(path.parse(__filename)); /* {
+//     root: 'D:\\',
+//     dir: 'D:\\NodeJsLearning',
+//     base: 'server.js',
+//     ext: '.js',
+//     name: 'server'
+//   }*/
